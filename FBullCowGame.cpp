ecd88076da50fcaf2de7fb47388c5e4d21a5a43b
Joshua Coles <joshuaocoles@gmail.com>
2018-09-19 23:26:44 +0200

    Update FBullCowGame.cpp

--- FBullCowGame.cpp
+++ FBullCowGame.cpp
@@ -1,8 +1,6 @@
 #include "FBullCowGame.h"
 
-
 using int32 = int;
-
 
 FBullCowGame::FBullCowGame() { Reset(); }
 
@@ -14,12 +12,12 @@ int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); 
 
 void FBullCowGame::Reset()
 {
-    constexpr int32 MaxTries = 5;
-    const FString HiddenWord = "planet";
+	constexpr int32 MaxTries = 5;
+	const FString HiddenWord = "planet";
 
-    MyMaxTries = MaxTries;
-    MyHiddenWord = HiddenWord;
-    MyCurrentTry = 1;
+	MyMaxTries = MaxTries;
+	MyHiddenWord = HiddenWord;
+	MyCurrentTry = 1;
 	return;
 }
 
@@ -32,27 +30,27 @@ bool FBullCowGame::IsGameWon() const
 
 EGuessStatus FBullCowGame::CheckValid(FString Guess) const
 {
-    // if the guess isn't an isogram
-    if (false)
-    {
-        return EGuessStatus::NotIsogram;
-    }
-    // if the guess isn't all lowercase
-    else if (false)
-    {
-        return EGuessStatus::NotLowercase;
-    }
-    // if guess length is wrong
-    else if (Guess.length() != GetHiddenWordLength())
-    {
-        return EGuessStatus::WrongLength;
-    }
-    // otherwise
-    else
-    {
-        // return OK
-        EGuessStatus::OK;
-    }
+	// if the guess isn't an isogram
+	if (false)
+	{
+		return EGuessStatus::NotIsogram;
+	}
+	// if the guess isn't all lowercase
+	else if (false)
+	{
+		return EGuessStatus::NotLowercase;
+	}
+	// if guess length is wrong
+	else if (Guess.length() != GetHiddenWordLength())
+	{
+		return EGuessStatus::WrongLength;
+	}
+	// otherwise
+	else
+	{
+		// return OK
+		return EGuessStatus::OK;
+	}
 }
 
 
@@ -63,7 +61,7 @@ FBCCount FBullCowGame::SubmitGuess(FString Guess)
 	MyCurrentTry++;
 
 	// setup a return variable
-    FBCCount BCCount;
+	FBCCount BCCount;
 
 	// loop through all letters in the guess
 	int32 HiddenWordLength = MyHiddenWord.length();
@@ -77,15 +75,15 @@ FBCCount FBullCowGame::SubmitGuess(FString Guess)
 			{
 				if (MHWChar == GChar)
 				{ // if they're in the same place
-                    BCCount.Bulls++; // incriment bulls
+					BCCount.Bulls++; // incriment bulls
 				}
 				else
 				{
-                    BCCount.Cows++; // must be a cow
+					BCCount.Cows++; // must be a cow
 				}
 			}
 		}
 	}
 
-    return BCCount;
+	return BCCount;
 }
