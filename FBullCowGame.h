0671bdb7829ead9155674c882e70d244639ef403
Joshua Coles <joshuaocoles@gmail.com>
2018-09-20 19:17:53 +0200

    Update FBullCowGame.h

--- FBullCowGame.h
+++ FBullCowGame.h
@@ -1,89 +1,46 @@
-#include "FBullCowGame.h"
+#pragma once
+#include <string>
 
+
+using FString = std::string;
 using int32 = int;
 
-FBullCowGame::FBullCowGame() { Reset(); }
 
-
-int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
-int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
-int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
-bool FBullCowGame::IsGameWon() const { return bGameIsWon; }
-
-
-void FBullCowGame::Reset()
+// all values intialised to zero
+struct FBCCount
 {
-	constexpr int32 MaxTries = 5;
-	const FString HiddenWord = "planet";
-
-	MyMaxTries = MaxTries;
-	MyHiddenWord = HiddenWord;
-	MyCurrentTry = 1;
-	bGameIsWon = false;
-	return;
-}
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
 
 
-EGuessStatus FBullCowGame::CheckValid(FString Guess) const
+enum EGuessStatus
 {
-	// if the guess isn't an isogram
-	if (false)
-	{
-		return EGuessStatus::NotIsogram; // TODO write function
-	}
-	// if the guess isn't all lowercase
-	else if (false)
-	{
-		return EGuessStatus::NotLowercase; // TODO write function
-	}
-	// if guess length is wrong
-	else if (Guess.length() != GetHiddenWordLength())
-	{
-		return EGuessStatus::WrongLength;
-	}
-	// otherwise
-	else
-	{
-		// return OK
-		return EGuessStatus::OK;
-	}
-}
+	InvalidStatus,
+	OK,
+	NotIsogram,
+	WrongLength,
+	NotLowercase
+};
 
 
-// receives a VALID guess, incriments turn, and returns count
-FBCCount FBullCowGame::SubmitValidGuess(FString Guess)
+class FBullCowGame
 {
-	MyCurrentTry++;
-	FBCCount BCCount;
-	int32 WordLength = MyHiddenWord.length(); // assuming same word as guess
+public:
+	FBullCowGame(); // constructor
 
-	// loop through all letters in the hidden word
-	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++)
-	{
-		// compare letters against the guess
-		for (int32 GChar = 0; GChar < WordLength; GChar++)
-		{
-			// if they match then
-			if (Guess[GChar] == MyHiddenWord[MHWChar])
-			{
-				if (MHWChar == GChar)
-				{ // if they're in the same place
-					BCCount.Bulls++; // incriment bulls
-				}
-				else
-				{
-					BCCount.Cows++; // must be a cow
-				}
-			}
-		}
-	}
-	if (BCCount.Bulls == WordLength)
-	{
-		bGameIsWon = true;
-	}
-	else
-	{
-		bGameIsWon = false;
-	}
-	return BCCount;
-}
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	bool IsGameWon() const;
+    EGuessStatus CheckValid(FString) const;
+
+	void Reset(); // TODO make a more rich return value.
+    FBCCount SubmitValidGuess(FString);
+
+private:
+	int32 MyCurrentTry;
+	int32 MyMaxTries;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+};
